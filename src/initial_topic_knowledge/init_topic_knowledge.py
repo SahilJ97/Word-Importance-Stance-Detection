d10123d0115ed8c85cf8e232e0b075e09cea4fef
Sahil Jayaram <sahil@Sahils-MBP.attlocal.net>
2021-03-08 15:43:17 -0800

    more efficient topic knowledge generation

--- src/initial_topic_knowledge/init_topic_knowledge.py
+++ src/initial_topic_knowledge/init_topic_knowledge.py
@@ -1,7 +1,4 @@
 from sys import argv
-from pytorch_pretrained_bert.tokenization import BertTokenizer
-from pytorch_pretrained_bert.modeling import BertModel
-from csv import DictReader
 import torch
 from kmeans_pytorch import kmeans
 import os
@@ -13,31 +10,30 @@ N_SLOTS, OUTPUT_FILE = argv[1], argv[2]
 N_SLOTS = int(N_SLOTS)
 EMBEDDINGS_FILE = "all_topic_embeddings.pt"
 
-train_set = VastReader(
-        "../../data/VAST/vast_train.csv",
-        "../../data/VAST_word_importance/token_appearances.tsv",
-        exclude_from_main="../../data/VAST_word_importance/special_datapoints.txt",
-        word_importance_csv="../../data/VAST_word_importance/processed_annotated.csv",
-        smoothing=None,
-        relevance_type="binary"
-    )
-model = BertJoint(doc_len=train_set.doc_len, fix_bert=True)
-
-token_type_ids = [0 for _ in range(train_set.doc_len + 2)] + [1 for _ in range(train_set.topic_len + 1)]
-token_type_ids = torch.tensor([token_type_ids], dtype=torch.long)
-
 
 if __name__ == "__main__":
     # Load embeddings if embeddings_file exists, otherwise compute them
     if os.path.exists(EMBEDDINGS_FILE):
         topic_embeddings = torch.load(EMBEDDINGS_FILE)
     else:
-        topic_embeddings = []
-        for item in train_set:
-            print(item)
-            inputs, _, doc_stopword_mask, topic_stopword_mask, _ = item
-            inputs = torch.unsqueeze(inputs, dim=0)
-            with torch.no_grad():
+        with torch.no_grad():
+            train_set = VastReader(
+                "../../data/VAST/vast_train.csv",
+                "../../data/VAST_word_importance/token_appearances.tsv",
+                exclude_from_main="../../data/VAST_word_importance/special_datapoints.txt",
+                word_importance_csv="../../data/VAST_word_importance/processed_annotated.csv",
+                smoothing=None,
+                relevance_type="binary"
+            )
+            model = BertJoint(doc_len=train_set.doc_len, fix_bert=True)
+            token_type_ids = [0 for _ in range(train_set.doc_len + 2)] + [1 for _ in range(train_set.topic_len + 1)]
+            token_type_ids = torch.tensor([token_type_ids], dtype=torch.long)
+            topic_embeddings = []
+            for i in range(len(train_set)):
+                if (i+1) % 100 == 0:
+                    print(f"Embedding {i+1}-th datapoint")
+                inputs, _, doc_stopword_mask, topic_stopword_mask, _ = train_set[i]
+                inputs = torch.unsqueeze(inputs, dim=0)
                 doc, topic = model.extract_co_embeddings(
                     get_pad_mask(inputs),
                     torch.unsqueeze(doc_stopword_mask, dim=0),
@@ -45,8 +41,8 @@ if __name__ == "__main__":
                     inputs=inputs,
                     token_type_ids=token_type_ids
                 )
-            topic_embeddings.append(topic)
-        topic_embeddings = torch.stack(topic_embeddings)
+                topic_embeddings.append(topic)
+            topic_embeddings = torch.stack(topic_embeddings)
         torch.save(topic_embeddings, EMBEDDINGS_FILE)
 
     # Cluster
