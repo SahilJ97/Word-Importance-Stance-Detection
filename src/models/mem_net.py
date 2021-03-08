8ea9dca5b5d6bab805845dd415d09f845ae3535a
Sahil Jayaram <sahil@Sahils-MBP.attlocal.net>
2021-03-07 18:31:01 -0800

    memnet working

--- src/models/mem_net.py
+++ src/models/mem_net.py
@@ -15,7 +15,7 @@ class MemoryNetwork(VastClassifier, ABC):
         super(MemoryNetwork, self).__init__(**kwargs)
         self.num_hops = num_hops
         self.knowledge_transfer_scheme = knowledge_transfer_scheme
-        self.M = torch.load(init_topic_knowledge_file,)
+        self.M = torch.load(init_topic_knowledge_file,)  # unsqueeze?
         self.W1 = torch.rand((768, 768),)
         self.W2 = torch.rand((768, 768),)
         if self.knowledge_transfer_scheme == "parallel":
@@ -63,9 +63,17 @@ class MemoryNetwork(VastClassifier, ABC):
                 results.append(h)
             return torch.cat(results)
 
+        elif self.knowledge_transfer_scheme == "parallel":
+            results = []
+            for h in topic_embedding, doc_embedding:
+                for hop in range(self.num_hops):
+                    h = shared_math(h)
+                results.append(h)
+            return torch.cat(results)
+
     def forward(self, pad_mask, doc_stopword_mask, topic_stopword_mask, inputs=None, inputs_embeds=None,
                 token_type_ids=None, **kwargs):
-        doc, topic = self.extract_co_embeddings(
+        doc_embeddings, topic_embeddings = self.extract_co_embeddings(
             pad_mask=pad_mask,
             doc_stopword_mask=doc_stopword_mask,
             topic_stopword_mask=topic_stopword_mask,
@@ -75,7 +83,10 @@ class MemoryNetwork(VastClassifier, ABC):
         )
 
         # Knowledge transfer component
-        H = self.knowledge_transfer(topic, doc)  # may need to fix that method! designed for one less dimension
+        H = []
+        for doc_emb, topic_emb in zip(doc_embeddings, topic_embeddings):
+            H.append(self.knowledge_transfer(topic_emb, doc_emb))
+        H = torch.stack(H)
 
         # Synthesizing component
         hl = torch.nn.functional.relu(
