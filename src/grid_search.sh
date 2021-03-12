c2c9b7ab57e1627195d8310015ca718694d79066
Sahil Jayaram <sahil@Sahils-MBP.attlocal.net>
2021-03-11 17:10:38 -0800

    new grid search

--- src/grid_search.sh
+++ src/grid_search.sh
@@ -1,11 +1,12 @@
 for seed in 0 1 2 3 4
 do
-  for k in 1 2 4
+  for k in 3 5
   do
     for relevance_scores in binary tf-idf
     do
-      for lambda in 4 8 16 32 64 128 256 512
+      for lambda in .5 1 2 4 8
       do
+        echo ...
         python3 train.py -r "$relevance_scores" -u y -b 32 -l 1e-5 -k "$k" --lambda "$lambda" -o model -m bert-joint -s "$seed" --gpu 1
       done
     done
